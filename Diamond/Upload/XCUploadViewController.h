49dbe9dfa7813bed041a064f8f225e37cd2a529d
xtra chen <xtrachen@msn.com>
2018-11-20 11:52:56 +0800

    add upload view

---
+++ Diamond/Upload/XCUploadViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCUploadViewController.h
+//  Diamond
+//
+//  Created by Xtra on 2018/11/20.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCUploadViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
