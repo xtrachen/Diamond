1d82526f39a1e3385771ad8be981a0120bb7484c
xtrachen <xtrachen@msn.com>
2018-11-29 00:58:09 +0800

    add detail view controller

---
+++ Diamond/Detail/XDDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  XDDetailViewController.h
+//  Diamond
+//
+//  Created by 陈国贤 on 2018/11/29.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Diamond/Detail/XDDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  XDDetailViewController.m
+//  Diamond
+//
+//  Created by 陈国贤 on 2018/11/29.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import "XDDetailViewController.h"
+
+@interface XDDetailViewController ()
+
+@end
+
+@implementation XDDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
