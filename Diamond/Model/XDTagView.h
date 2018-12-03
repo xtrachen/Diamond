6855f79e8be66ba0a32f986b691dc916c11f78eb
xtrachen <xtrachen@msn.com>
2018-12-04 00:25:47 +0800

    add view

---
+++ Diamond/Model/XDTagView.h
@@ -0,0 +1,17 @@
+//
+//  XDTagView.h
+//  Diamond
+//
+//  Created by 陈国贤 on 2018/12/4.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDTagView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
