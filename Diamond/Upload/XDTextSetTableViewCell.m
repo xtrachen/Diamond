a7a1da89a7b2912206e561adfe786cccac7fc897
xtra chen <xtrachen@msn.com>
2018-12-03 18:44:45 +0800

    add view

---
+++ Diamond/Upload/XDTextSetTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  XDTextSetTableViewCell.m
+//  Diamond
+//
+//  Created by Xtra on 2018/12/3.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import "XDTextSetTableViewCell.h"
+
+@implementation XDTextSetTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
