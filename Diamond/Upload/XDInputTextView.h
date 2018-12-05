f103975b7d6b632893adceb301a7030e76607ccf
xtra chen <xtrachen@msn.com>
2018-12-05 21:54:25 +0800

    add input text View

---
+++ Diamond/Upload/XDInputTextView.h
@@ -0,0 +1,17 @@
+//
+//  XDInputTextView.h
+//  Diamond
+//
+//  Created by Xtra on 2018/12/5.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDInputTextView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Diamond/Upload/XDInputTextView.m
@@ -0,0 +1,21 @@
+//
+//  XDInputTextView.m
+//  Diamond
+//
+//  Created by Xtra on 2018/12/5.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import "XDInputTextView.h"
+
+@implementation XDInputTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
