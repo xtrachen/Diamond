bdad1b5ca568e55d8f4cbcbbc7d3313e6ea9a4e3
xtra chen <xtrachen@msn.com>
2018-12-21 22:35:44 +0800

    add view

---
+++ Diamond/Model/XDTimeupButtonView.h
@@ -0,0 +1,17 @@
+//
+//  XDTimeupButtonView.h
+//  Diamond
+//
+//  Created by Xtra on 2018/12/21.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDTimeupButtonView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Diamond/Model/XDTimeupButtonView.m
@@ -0,0 +1,21 @@
+//
+//  XDTimeupButtonView.m
+//  Diamond
+//
+//  Created by Xtra on 2018/12/21.
+//  Copyright © 2018年 XtraSoft. All rights reserved.
+//
+
+#import "XDTimeupButtonView.h"
+
+@implementation XDTimeupButtonView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
